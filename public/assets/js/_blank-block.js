18ef3201f00d52568d39ad50d5b23560ac04665f
Yosshy <Yosshy_123@proton.me>
2026-02-02 19:49:29 +0900

    commit

--- public/assets/js/_blank-block.js
+++ public/assets/js/_blank-block.js
@@ -1,11 +1,12 @@
 document.addEventListener("click", e => {
   const a = e.target.closest("a");
-  if (a && a.target === "_blank") {
+  if (a && a.href) {
     e.preventDefault();
     window.location.href = a.href;
   }
+
   const f = e.target.closest("form");
-  if (f && f.target === "_blank") {
+  if (f && f.action) {
     e.preventDefault();
     f.target = "_self";
     f.submit();
