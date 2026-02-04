a992472e25a201cd69423d9259a2d4eb5c1a601e
Yosshy <Yosshy_123@proton.me>
2026-02-04 18:14:27 +0900

    コード整理

--- index.js
+++ index.js
@@ -3,24 +3,17 @@ import http from 'node:http';
 import { createBareServer } from "@tomphttp/bare-server-node";
 import cors from 'cors';
 import path from 'node:path';
-import { hostname } from "node:os"
 
 const server = http.createServer();
-const app = express(server);
-const __dirname = process.cwd();
+const app = express();
+const rootDir = process.cwd();
 const bareServer = createBareServer('/bare/');
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
-app.use("uv", express.static(__dirname + '/uv'));
-app.use(cors());
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), '/public/index.html'));
-});
+app.use(express.static(path.join(rootDir, "public")));
 
 server.on('request', (req, res) => {
   if (bareServer.shouldRoute(req)) {
@@ -38,17 +31,18 @@ server.on('upgrade', (req, socket, head) => {
   }
 })
 
-server.on('listening', () => {
-  const address = server.address();
-})
-
-server.listen({ port: PORT, })
+server.listen(PORT, () => {
+  console.log(`Server Listening on ${PORT}`);
+});
 
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
 
 function shutdown() {
-  server.close();
-  bareServer.close();
-  process.exit(0);
+  console.log("Shutting down...");
+
+  server.close(() => {
+    bareServer.close();
+    process.exit(0);
+  });
 }
